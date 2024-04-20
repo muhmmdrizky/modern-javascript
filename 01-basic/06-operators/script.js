de5427afa3392edb2a3a4e043d9383fbad465da5
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-21 06:13:15 +0700

    init

---
+++ 01-basic/06-operators/script.js
@@ -0,0 +1,53 @@
+// 1. Arithmetic Operators
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// Concatenation
+x = "Hello" + ", " + "World";
+
+// Exponent
+x = 5 ** 5;
+
+// Increment
+x = 1;
+x++; // x + 1
+
+// Decrement
+x = 2;
+x--;
+
+// 2. Assignment Operators
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+
+// 3. Comparison Operators
+// Equal to (Just the value, not the type)
+x = 2 == "2";
+
+// Equal to (Type and value)
+x = 2 === "2";
+
+// Not equal to (Just the value, not the type)
+x = 2 != "2";
+
+// Not equal to (Type and value)
+x = 2 !== 2;
+
+// Greater than and less than
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
