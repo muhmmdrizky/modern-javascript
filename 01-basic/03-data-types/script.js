a4ca63df95ed3ec7cdea702eb8396cfe0158f656
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-21 05:40:52 +0700

    init

---
+++ 01-basic/03-data-types/script.js
@@ -0,0 +1,35 @@
+// String
+const name = "Muhammad Rizky";
+
+// Number
+const age = 33;
+const temp = 95.5;
+
+// Boolean
+const isAdmin = true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+const score = undefined;
+
+// Symbol
+const id = Symbol("id");
+
+// BigInt
+const n = 9007199254740991n;
+
+// Reference types
+const numbers = [1, 2, 3, 4, 5];
+
+const person = {
+  name: "Ahmad",
+};
+
+function sayHello() {
+  console.log("Hello World.");
+}
+
+const output = sayHello;
+console.log(output, typeof output);
