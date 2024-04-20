773201fffc22cb581dad244de47735bfff726144
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-21 06:04:36 +0700

    init

---
+++ 01-basic/05-type-conversion/script.js
@@ -0,0 +1,19 @@
+// Convert string to number
+let stringToNumber = "55.5";
+stringToNumber = parseInt(stringToNumber);
+console.log(stringToNumber);
+
+// Convert number to string
+let numberToString = 55;
+numberToString = numberToString.toString();
+console.log(numberToString);
+
+// Convert string to decimal
+let stringToDecimal = "55.5";
+stringToDecimal = parseFloat(stringToDecimal);
+console.log(stringToDecimal);
+
+// Convert number to boolean
+let numberToBoolean = 55;
+numberToBoolean = Boolean(numberToBoolean);
+console.log(numberToBoolean);
