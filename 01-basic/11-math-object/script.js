75c278d03ca4dac8d496bc869591a4eb61facc47
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-29 06:54:20 +0700

    init

---
+++ 01-basic/11-math-object/script.js
@@ -0,0 +1,34 @@
+let x;
+
+// Square root
+x = Math.sqrt(9);
+
+// Absolute value
+x = Math.abs(-5);
+
+// Round
+// Angka di belakang koma < 5 ? dibuletin ke bawah : dibuletin ke atas
+x = Math.round(5.4);
+
+// Round up
+x = Math.ceil(5.2);
+
+// Round down
+x = Math.floor(5.9);
+
+// Exponent
+x = Math.pow(2, 3); //8
+
+// Minimum number
+x = Math.min(5, 6, 7);
+
+// Maximum number
+x = Math.max(5, 6, 7);
+
+// Get a random number/decimal between 0 and 1
+x = Math.random();
+
+// Get a random number between 1 and 100
+x = Math.random() * 100 + 1;
+
+console.log(x);
