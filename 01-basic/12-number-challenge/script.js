c2085d6de6672d2f69af70ae6535923abb1902d1
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-29 07:15:18 +0700

    init

---
+++ 01-basic/12-number-challenge/script.js
@@ -0,0 +1,24 @@
+const x = Math.random() * 1 + 100;
+const y = Math.random() * 1 + 50;
+
+// Get the sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+
+// Get the difference
+const difference = x - y;
+const differenceOutput = `${x} - ${y} = ${difference}`;
+
+// Get the product
+const product = x * y;
+const productOutput = `${x} * ${y} = ${product}`;
+
+// Get the quotient
+const quotient = x / y;
+const quotientOutput = `${x} / ${y} = ${quotient}`;
+
+// Get the remainder
+const remainder = x % y;
+const remainderOutput = `${x} % ${y} = ${remainder}`;
+
+console.log(remainderOutput);
